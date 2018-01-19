bc94a3b4cc57f4e263e37f0ecf5c5e5d9cdcd023
tufei <1024522245@qq.com>
2018-01-19 20:58:44 +0800

    增加Version类

---
+++ okhttp/src/main/java/okhttp3/internal/Version.java
@@ -0,0 +1,15 @@
+package okhttp3.internal;
+
+/**
+ * @author tufei
+ * @date 2018/1/19.
+ */
+
+public final class Version {
+    public static String userAgent() {
+        return "okhttp/3.8.1";
+    }
+
+    private Version() {
+    }
+}
